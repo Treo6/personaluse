840d77a8b176a3b456f646abb5bee20d28120b09
Treo6 <138954824+Treo6@users.noreply.github.com>
2023-12-04 01:27:28 +0800

    Update bilibili.js

--- QuantumultX/bilibili.js
+++ QuantumultX/bilibili.js
@@ -11,4 +11,12 @@ hostname = *.biliapi.*, *.bilibili.*
 
 */
 
-document.write(unescape('var%20modifiedHeaders%20%3D%20%24request.headers%3B%0A%0A//modifiedHeaders%5B%27Cookie%27%5D%20%3D%20%27%27%3B%0A%0AmodifiedHeaders%5B%27Authorization%27%5D%20%3D%20%27identify_v1%20a9e10cbcd8bf28273877ac49afe6ebb1CjCnc_CjQHJp-Ryy1AFZ1u35E7PSFpdjQO8zB0jXFfGlMMGiZwpQPUPIzWEyKkLjxQwSVmhjckRoZWxfampQaEx4OTA5LWhOU19zaldKdU5mak9uT21EUjhNa2pnRUhZSzh5ZzdRZ21oMGxmTVF6WkxxcFA0dkFmMUxHajlGTUo5WlVfVzZmbVhnIIEC%27%3B%0A%0AmodifiedHeaders%5B%27User-Agent%27%5D%20%3D%20%27bili-universal/75700100%20os/ios%20model/iPhone%2015%20Pro%20Max%20mobi_app/iphone%20osVer/17.0.3%20network/2%20grpc-objc-cronet/1.47.0%20grpc-c/25.0.0%20%28ios%3B%20cronet_http%29%27%3B%0A%0A%24done%28%7Bheaders%20%3A%20modifiedHeaders%7D%29%3B'));
+var modifiedHeaders = $request.headers;
+
+//modifiedHeaders['Cookie'] = '';
+
+modifiedHeaders['Authorization'] = 'identify_v1 a9e10cbcd8bf28273877ac49afe6ebb1CjCnc_CjQHJp-Ryy1AFZ1u35E7PSFpdjQO8zB0jXFfGlMMGiZwpQPUPIzWEyKkLjxQwSVmhjckRoZWxfampQaEx4OTA5LWhOU19zaldKdU5mak9uT21EUjhNa2pnRUhZSzh5ZzdRZ21oMGxmTVF6WkxxcFA0dkFmMUxHajlGTUo5WlVfVzZmbVhnIIEC';
+
+modifiedHeaders['User-Agent'] = 'bili-universal/75700100 os/ios model/iPhone 15 Pro Max mobi_app/iphone osVer/17.0.3 network/2 grpc-objc-cronet/1.47.0 grpc-c/25.0.0 (ios; cronet_http)';
+
+$done({headers : modifiedHeaders});
