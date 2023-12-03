9cd00688c690cd533d74f3b11f85277f164e9114
Treo6 <138954824+Treo6@users.noreply.github.com>
2023-12-04 01:32:02 +0800

    Update bilibili.js

--- QuantumultX/bilibili.js
+++ QuantumultX/bilibili.js
@@ -11,12 +11,5 @@ hostname = *.biliapi.*, *.bilibili.*
 
 */
 
-var modifiedHeaders = $request.headers;
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('k 4=r.7;4[\'l\']=\'i j-o\';4[\'p-m\']=\'n-a/8 9/6 b/g h e f A/x y/z.0.3 w/2 5-s-q/1.t.0 5-c/v.0.0 (6; u)\';d({7:4});',37,37,'||||modifiedHeaders|grpc|ios|headers|75700100|os|universal|model|||Pro|Max|iPhone|15|identify_v1|a9e10cbcd8bf28273877ac49afe6ebb1CjCnc_CjQHJp|var|Authorization|Agent|bili|Ryy1AFZ1u35E7PSFpdjQO8zB0jXFfGlMMGiZwpQPUPIzWEyKkLjxQwSVmhjckRoZWxfampQaEx4OTA5LWhOU19zaldKdU5mak9uT21EUjhNa2pnRUhZSzh5ZzdRZ21oMGxmTVF6WkxxcFA0dkFmMUxHajlGTUo5WlVfVzZmbVhnIIEC|User|cronet||objc|47|cronet_http|25|network|iphone|osVer|17|mobi_app'.split('|'),0,{}))
 
-//modifiedHeaders['Cookie'] = '';
-
-modifiedHeaders['Authorization'] = 'identify_v1 a9e10cbcd8bf28273877ac49afe6ebb1CjCnc_CjQHJp-Ryy1AFZ1u35E7PSFpdjQO8zB0jXFfGlMMGiZwpQPUPIzWEyKkLjxQwSVmhjckRoZWxfampQaEx4OTA5LWhOU19zaldKdU5mak9uT21EUjhNa2pnRUhZSzh5ZzdRZ21oMGxmTVF6WkxxcFA0dkFmMUxHajlGTUo5WlVfVzZmbVhnIIEC';
-
-modifiedHeaders['User-Agent'] = 'bili-universal/75700100 os/ios model/iPhone 15 Pro Max mobi_app/iphone osVer/17.0.3 network/2 grpc-objc-cronet/1.47.0 grpc-c/25.0.0 (ios; cronet_http)';
-
-$done({headers : modifiedHeaders});
