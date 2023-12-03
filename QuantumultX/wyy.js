5ead2635cbd5d6a0af9176abd189dd085b243636
Treo6 <138954824+Treo6@users.noreply.github.com>
2023-12-04 00:59:49 +0800

    Update wyy.js

--- QuantumultX/wyy.js
+++ QuantumultX/wyy.js
@@ -1,9 +1,30 @@
 /*
-致敬伟人
+作者:汌（大吗喽）
 
 [rewrite_local]
 # > 网易云解锁VIP
 ^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 播放器会员皮肤
+^https:\/\/interface3\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 搜索结果会员歌曲
+^https:\/\/interface3\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 播放器会员歌曲
+^https:\/\/interface3\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|type\/detail\/get) url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+^https:\/\/interface3\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 侧边栏会员等级
+^https:\/\/interface3\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 首页歌单会员歌曲
+^https?:\/\/interface3\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+
+# 会员认证
+^https?:\/\/interface3\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header https://raw.githubusercontent.com/Treo6/personaluse/main/QuantumultX/wyy.js
+ 
+
 [mitm] 
 hostname = *.music.163.com
 
